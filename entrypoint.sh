28a3e1d1c85ce70ab22acbadf8c24d4e37541c5b
Lucian T <35923115+luciantanase14@users.noreply.github.com>
2024-02-05 16:23:24 +0200

    Update entrypoint.sh
    udpating sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # entrypoint.sh
 
-# Read the secret
-export SALT=$(cat /run/secrets/app_salt)
+# Checking if the app_salt secret exists and read it
+if [ -f /run/secrets/app_salt ]; then
+    export SALT=$(cat /run/secrets/app_salt)
+else
+    echo "Warning: /run/secrets/app_salt not found. Using default or environment-provided SALT value."
+fi
 
 # Execute the main container command
 exec "$@"
