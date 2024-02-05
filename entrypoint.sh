da23f1a3f8b60c01c5c395e3c278549d04cb567d
Lucian T <35923115+luciantanase14@users.noreply.github.com>
2024-02-05 16:36:30 +0200

    Update entrypoint.sh
    improving for security reasons

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # entrypoint.sh
 
-# Checking if the app_salt secret exists and read it
+# Check if the app_salt secret exists and use it if present:
 if [ -f /run/secrets/app_salt ]; then
     export SALT=$(cat /run/secrets/app_salt)
-else
-    echo "Warning: /run/secrets/app_salt not found. Using default or environment-provided SALT value."
+# Check if SALT is already set as an environment variable:
+elif [ -z "${SALT}" ]; then
+    # Generate a secure SALT value if not provided:
+    export SALT=$(openssl rand -hex 16)
+    # Redirect the output of the echo command to /dev/null to suppress it:
+    echo "Generated a new SALT value" >/dev/null
 fi
 
 # Execute the main container command
