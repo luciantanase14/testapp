d91b4aa2e8886a29f029559939ef4d62f5926dc7
Lucian T <35923115+luciantanase14@users.noreply.github.com>
2024-02-06 11:05:22 +0200

    Update entrypoint.sh
    improving security entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,8 @@
 
 # Check if the SALT environment variable is empty
 if [ -z "$SALT" ]; then
-    echo "No SALT provided. Generating a random SALT."
     SALT=$(openssl rand -hex 16)
     export SALT
-    # Removed the line that logs the generated SALT to improve security
 fi
 
 # Start the Go application with the SALT value
